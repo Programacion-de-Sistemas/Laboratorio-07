b663354e89713edc06c31f8c5a463004ab0d8355
JuanPastor <jpastorp@unsa.edu.pe>
2022-06-26 11:34:47 -0500

    Se prueba el ejercicio dado por el profesor

--- Ejercicio2.c
+++ Ejercicio2.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
+int mult_ok(int x, int y) {
+    int p = x*y;
+    /* x es 0, o dividir p por x = y */
+    return !x || p/x == y;
+}
 int main() {
-  printf("Hello World!");
-  return 0;
+    mult_ok(5, 6);
 }
