731eb4b2e0295d3e4fe4f15fe577e7cb373f6e48
JuanPastor <jpastorp@unsa.edu.pe>
2022-06-26 13:13:41 -0500

    Se arreglaron los problemas con los impares restando uno al contador cuando es impar y se creo una funcion de valor absoluto

--- Ejercicio2.c
+++ Ejercicio2.c
@@ -1,17 +1,33 @@
 #include <stdio.h>
 #include <stdint.h>
+int64_t absoluto(int64_t numero){
+    if(numero<0){
+        return numero - (numero*2);
+    }
+    else{
+        return numero;
+    }
+}
 int mult_ok(int64_t x, int64_t y) {
+    int64_t absx = absoluto(x);
+    int64_t absy = absoluto(y);
     int64_t contadorx = 1;
     int64_t contadory = 1;
     int64_t auxx = 1;
     int64_t auxy = 1;
-    while(auxx <= x){
+    while(auxx <= absx){
         auxx = auxx*2;
         contadorx = contadorx + 1;
     }
-     while(auxy <= y){
+     while(auxy <= absy){
         auxy = auxy*2;
         contadory = contadory + 1;
+    }
+    if(x%2 != 0){
+        contadorx--;
+    }
+    if(y%2 != 0){
+        contadory--;
     }
     
     return (contadorx+contadory) < 64;
