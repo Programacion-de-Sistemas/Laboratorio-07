6fc8351d649df19e0cf3c3798a2ad2eb413d6889
JuanPastor <jpastorp@unsa.edu.pe>
2022-06-27 07:12:04 -0500

    Se intento el ejercicio 1

--- Ejercicio1.c
+++ Ejercicio1.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 int bis(int x, int m){
-    int[] listax = new int[32];
+    int listax[] = new int[32];
         for(int i = 0; i < 32; i++){
             listax[i]=0;
         }
     return bisn(x, m, listax, 31);
 }    
-int bisn(int x, int m, int[] lista, int contador){
+int bisn(int x, int m, int lista[], int contador){
     if(x != 1 && y != 1){
         int bitx = x%2;
         int bitm = m%2;
@@ -15,11 +15,11 @@ int bisn(int x, int m, int[] lista, int contador){
             res = 1;
         } 
         else{
-            res = bitx
+            res = bitx;
         }
         bisn(x/2, m/2, lista, contador);
         lista[contador] = res;
-        printf("lista" + "%i %i" + contador, res)
+        printf("lista" + "%i %i" + contador, res);
         contador--;
         
     }
@@ -27,14 +27,14 @@ int bisn(int x, int m, int[] lista, int contador){
 }
 int bic(int x, int m);
 /* Calcular x|y usando solo llamadas a las funciones bis y bic */
-int bool_or(int x, int y) {
+/*int bool_or(int x, int y) {
     int resultado = __________ ;
     return resultado;
 }
 int bool_xor(int x, int y) {
     int resultado = __________;
     return resultado;
-}
+}*/
 int main(){
-    bis(7,9)
+    bis(7,9);
 }
