bc6932e6ff8ba2f2fa0ae9f86f240503c560f918
JuanPastor <jpastorp@unsa.edu.pe>
2022-07-03 17:56:31 -0500

    Se encontro en un error al hacer la prueba pero se solucion con una virgulilla faltante

--- juan pedro pastor - datalab-handout/datalab-handout/pruebas.c
+++ juan pedro pastor - datalab-handout/datalab-handout/pruebas.c
@@ -1,7 +1,8 @@
-
+#include <stdio.h>
 int bitXor(int x, int y) {
-    printf("%i\n", ~(~(~x & y) & ~(x & y)));
-  return ~(~(~x & y) & ~(x & y));
+    printf("%i\n",x^y);
+    printf("%i\n", ~(~(~x & y) & ~(x & ~y)));
+  return ~(~(~x & y) & ~(x & ~y));
 }
 int main(){
     bitXor(4,5);
