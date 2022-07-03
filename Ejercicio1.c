05251599c37742d706fe3153e3317d61955f5c11
JuanPastor <jpastorp@unsa.edu.pe>
2022-07-03 17:07:05 -0500

    Se copio correctamente el contenido del trabajo

--- Ejercicio1.c
+++ Ejercicio1.c
@@ -1,13 +1,6 @@
 #include <stdio.h>
-int bis(int x, int m){
-    int listax[] = new int[32];
-        for(int i = 0; i < 32; i++){
-            listax[i]=0;
-        }
-    return bisn(x, m, listax, 31);
-}    
 int bisn(int x, int m, int lista[], int contador){
-    if(x != 1 && y != 1){
+    if(x != 1 && m != 1){
         int bitx = x%2;
         int bitm = m%2;
         int res;
@@ -19,12 +12,21 @@ int bisn(int x, int m, int lista[], int contador){
         }
         bisn(x/2, m/2, lista, contador);
         lista[contador] = res;
-        printf("lista" + "%i %i" + contador, res);
+        printf("lista  %i %i\n", contador, res);
         contador--;
         
     }
 
 }
+int bis(int x, int m){
+    int listax[32];
+        for(int i = 0; i < 32; i++){
+            listax[i]=0;
+        }
+    int resultado = bisn(x, m, listax, 31);
+    return resultado;
+}    
+
 int bic(int x, int m);
 /* Calcular x|y usando solo llamadas a las funciones bis y bic */
 /*int bool_or(int x, int y) {
