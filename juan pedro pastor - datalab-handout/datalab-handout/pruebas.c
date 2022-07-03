7ec1e5e95d149ea5c1c593e8b0e7fc7609461831
JuanPastor <jpastorp@unsa.edu.pe>
2022-07-03 18:39:48 -0500

    re reqalizo una prueba en la que veriamos si ~ cambia al ultimo bit tambien

--- juan pedro pastor - datalab-handout/datalab-handout/pruebas.c
+++ juan pedro pastor - datalab-handout/datalab-handout/pruebas.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int bitXor(int x, int y) {
-    printf("%i\n",x^y);
-    printf("%i\n", ~(~(~x & y) & ~(x & ~y)));
+    printf("%i\n",~x);
+    
   return ~(~(~x & y) & ~(x & ~y));
 }
 int main(){
